d04ad0ed763202ac1264ac317453e282017e0fe5
leandrobolla <81895500+leandrobolla@users.noreply.github.com>
2024-09-23 15:33:13 -0300

    Add files via upload

---
+++ import numpy.py
@@ -0,0 +1,6 @@
+import numpy
+
+
+import array as ar
+matriz = ar.array('i',[1,2,3,4,5]) # la 'i' es de INT y 'd' se usaria para decimales.
+print(matriz)
---
+++ listasuper = [].py
@@ -0,0 +1,6 @@
+letras = ["def", "abc","xyz"]
+
+#letras.sort()
+# sort ordena las listas, alfabeticamente, o numericamente. sort reverse hace lo mismo a la inversa.
+eliminar = letras.pop ("abc")
+print (eliminar)
